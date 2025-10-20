435feaf6ff319917400781e4c2f527d325ed5d6c
ozcelikcode <75310949+ozcelikcode@users.noreply.github.com>
2025-10-20 18:43:58 +0300

    Create strings.py

---
+++ python-course/strings.py
@@ -0,0 +1,25 @@
+print("Kaçırma işlemi: ", 'Free \'Palestine') # buradaki kaçırma yapmak için ters slash yapılır.
+
+print("3'lü çift tırnak kullanımı: ", """Selam
+      
+      
+      
+      Nasılsın?
+      
+      
+      
+      Dünya""") # (""" """) 6 adet çift tırnak içerisinde yazılanlar boşluklar ve satırlar dahil edilir.
+
+print("Satır atlama işlemi: ", "Satır \natlama") # \n -> satır atlar
+
+print("Cümle TAB işlemi: ", "Cümle \t\t\tatlama")
+
+
+
+## Değişkenler
+
+comment = "Hello"
+space = " "
+comment2 = "World!"
+
+print("Çoklu değişken kullanma: ", comment + space + comment2 + " Text" + comment[3] + comment[-2])
