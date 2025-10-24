65d053dccf9f2bf2bb2e0e470ae17645373ff705
ozcelikcode <75310949+ozcelikcode@users.noreply.github.com>
2025-10-24 12:04:53 +0300

    Create dictionaries.py

---
+++ python-course/dictionaries.py
@@ -0,0 +1,34 @@
+# Dictionaries
+
+example = {
+    "Name" : "Suleyman",
+    "Age" : 21,
+    "Hobies" :  [
+        "Cinema",
+        "Software Coding",
+        "Reading Quran"
+    ]
+}
+
+print(example["Name"]) # name'i getir
+
+example.update({"Name" : "Gülsena", "Age" : 24}) # name'i değiştir
+print(example["Name"])
+
+example["id"] = 1 # id etiketi dıştan eklendi
+print(example)
+
+del example["id"] # sil
+print(example)
+
+
+print(example.keys())
+
+
+# Değeri tam ve alt alta gösterme
+for k,v in example.items():
+    print(k,v)
+
+
+# Değer mevcut mu?
+print(example.get("id", "Not Found")) # bulunursa, bulunmazsa
