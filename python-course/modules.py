8aca3cbf8b323f7210422e68456086ce6ef8c621
ozcelikcode <75310949+ozcelikcode@users.noreply.github.com>
2025-12-27 08:50:44 +0300

    Create modules.py

---
+++ python-course/modules.py
@@ -0,0 +1,9 @@
+import math # matematik import edildi
+
+conclusion = math.factorial(8)
+print(conclusion)
+
+from math import factorial # math fonksiyonundaki faktoriyel sadece import edildi. * (yıldız) bütün math fonksiyonu içindekilerini alır
+print(factorial(6))
+
+# Özel olarak import etme: import OZEL_DOSYA as MODUL ISMI
