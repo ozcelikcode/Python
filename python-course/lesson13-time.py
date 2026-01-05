2f6c6a9518a4a685a6c9049eafecb0c6ac32b812
ozcelikcode <75310949+ozcelikcode@users.noreply.github.com>
2026-01-05 11:54:08 +0300

    Update lesson13-time.py

--- python-course/lesson13-time.py
+++ python-course/lesson13-time.py
@@ -3,9 +3,18 @@ import time
 times = time.time()
 print(times)
 
-start = time.time()
-list = []
-for i in range(10):
-    list.append(i)
-finish = time.time()
-print(finish - start)
+start_time = time.time()
+my_list = []
+for i in range(100000):
+    my_list.append(i)
+finish_time = time.time()
+print(finish_time - start_time)
+
+
+times2 = time.strftime("%d:%m:%Y %H:%M:%S")
+print(times2)
+
+
+print("Yükleniyor...")
+time.sleep(5)
+print("Tamamlandı.")
