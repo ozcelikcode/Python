bb3f8cde9b351c36252eaed7c1343d9a2368b2da
ozcelikcode <75310949+ozcelikcode@users.noreply.github.com>
2026-01-07 11:26:48 +0300

    Create lesson15-os-module.py

---
+++ python-course/lesson15-os-module.py
@@ -0,0 +1,26 @@
+import os
+
+print(os.getcwd())
+# os.chdir("C:\Users\ozcelik\Documents\")
+# print(os.getcwd())
+
+
+for file in os.listdir():
+    print("- ", file) # dosyaları satırlara diz
+
+# oluştur ? klasör bir kere oluşturulabilir, 2. denemede hata verir
+os.mkdir("bu-klasor-python-tarafindan-olusturuldu")
+os.makedirs("Klasor-1/Klasor-2/Klasor-3")
+
+# sil
+os.rmdir("Klasor-1/Klasor-2/Klasor-3")
+# os.removedirs("Klasor-1/Klasor-2") # hata verir
+
+os.remove("GEREKSIZ_DOSYA.txt")
+
+# değiştir
+os.rename("lists.py", "lesson3-lists.py")
+
+os.rename("GEREKSIZ_DOSYA.txt", "\python-course\Klasor-1") # taşınır
+
+# konu hakkında daha fazla bilgi için: https://www.youtube.com/watch?v=FwpD9m9jWRo&list=PPSV
