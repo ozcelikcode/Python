4227f687319108166495a55c89dcd7b35afdbdbe
ozcelikcode <75310949+ozcelikcode@users.noreply.github.com>
2026-05-05 20:43:45 +0300

    Update 01_functions_practice.py

--- python-course/exercises/04_functions/01_functions_practice.py
+++ python-course/exercises/04_functions/01_functions_practice.py
@@ -19,12 +19,21 @@ greet("Sam")
 #   result = square(5)     → dönen değeri result'a atar
 #   print(result)          → 25 yazar
 
+def square(num):
+    return num * num
+result = square(15)
+print(result) # 225
+
 
 # Alıştırma 3: Birden fazla parametre
 # Anlamı:
 #   def add(a, b):         → iki parametre alır
 #       return a + b       → toplamı döndürür
 #   print(add(3, 7))       → 10 yazar
+
+def add(a, b):
+    return a / b
+print(add(10, 2)) # a, b => 10 / 2 = 5.0
 
 
 # Alıştırma 4: Liste alan fonksiyon
