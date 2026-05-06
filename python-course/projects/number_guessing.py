d3cc43f3ef51feffa0bd187a381eacdbcb39e01d
ozcelikcode <75310949+ozcelikcode@users.noreply.github.com>
2026-05-06 22:10:21 +0300

    Update number_guessing.py

--- python-course/projects/number_guessing.py
+++ python-course/projects/number_guessing.py
@@ -32,6 +32,15 @@ attempts = 0
 #           print(f"Doğru! {attempts} tahminde buldun.")
 #           break                        → doğru tahmin → döngüyü bitir
 
+difference = abs(guess - secret)
+
+if difference <= 5:
+    print("Çok yakınsın!")
+elif difference <= 20:
+    print("Yakınsın!")
+else:
+    print("Uzaksın")
+
 while True:
     guess = int(input("Tahmin: "))
     attempts += 1
