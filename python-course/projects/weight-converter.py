ab84a68ff22fc32ab1d26180903bb290dd6d1f72
ozcelikcode <75310949+ozcelikcode@users.noreply.github.com>
2026-05-25 22:54:01 +0300

    Update weight-converter.py

--- python-course/projects/weight-converter.py
+++ python-course/projects/weight-converter.py
@@ -4,10 +4,10 @@ unit = input("Kilogram or Pounds (K or L): ")
 if unit == "K":
     weight = weight * 2.205
     unit = "Lbs."
+    print(f"Your weights is: {round(weight, 1)} {unit}")
 elif unit == "L":
     weight = weight / 2.205
     unit = "Kgs."
+    print(f"Your weights is: {round(weight, 1)} {unit}")
 else:
     print(f"{unit} was not valid")
-
-print(f"Your weights is: {round(weight, 1)} {unit}")
