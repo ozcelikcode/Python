989613c2ad94e5f9f3e0157bcbe43a03539320ab
ozcelikcode <75310949+ozcelikcode@users.noreply.github.com>
2026-05-25 18:58:18 +0300

    Create basic-calculator.py

---
+++ python-course/projects/basic-calculator.py
@@ -0,0 +1,19 @@
+
+operator = input("Enter your operator (+, -, *, /): ")
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+if operator == "+":
+    result = num1 + num2
+    print(round(result, 3))
+elif operator == "-":
+    result = num1 - num2
+    print(round(result, 3))
+elif operator == "*":
+    result = num1 * num2
+    print(round(result, 3))
+elif operator == "/":
+    result = num1 / num2
+    print(round(result, 3))
+else:
+    print(f"{operator} is not valid operator")
