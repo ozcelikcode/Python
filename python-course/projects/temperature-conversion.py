7ac3324f75658d78fa4bc82530df814ae0b4ac7d
ozcelikcode <75310949+ozcelikcode@users.noreply.github.com>
2026-05-26 11:13:55 +0300

    Update temperature-conversion.py

--- python-course/projects/temperature-conversion.py
+++ python-course/projects/temperature-conversion.py
@@ -1,10 +1,10 @@
 unit = input("Is this temperature in celcius or fahrenheit (C / F): ")
 temp = float(input("Enter the temperature: "))
 
-if unit == "C":
+if unit == "C" or unit == "c":
     temp = round ((9 * temp) / 5 + 32, 1)
     print(f"The temperature in Fahrenheit is: {temp}°F")
-elif unit == "F":
+elif unit == "F" or unit == "f":
     temp = round((temp - 32) * 5 / 9, 1)
     print(f"The temperature in celcius is: {temp}°C")
 else:
