a002684d20c8d6127b371ea99284a8c1e4ea7467
ozcelikcode <75310949+ozcelikcode@users.noreply.github.com>
2026-06-01 12:07:42 +0300

    Update compound-interest-calculator.py

--- python-course/projects/compound-interest-calculator.py
+++ python-course/projects/compound-interest-calculator.py
@@ -7,4 +7,9 @@ while principle <= 0:
     if principle <= 0:
         print("Principle can't be less than or equal to zero")
 
+while rate <= 0:
+    rate = float(input("Enter the rate amount: "))
+    if rate <= 0:
+        print("Rate can't be less than or equal to zero")
+
 print(principle)
