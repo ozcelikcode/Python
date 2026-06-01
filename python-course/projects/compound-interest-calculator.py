048ea335d5f6a6763effb933be84d2b5087a4007
ozcelikcode <75310949+ozcelikcode@users.noreply.github.com>
2026-06-01 12:22:32 +0300

    Update compound-interest-calculator.py

--- python-course/projects/compound-interest-calculator.py
+++ python-course/projects/compound-interest-calculator.py
@@ -2,14 +2,20 @@ principle = 0
 rate = 0
 time = 0
 
-while principle <= 0:
+while principle < 0:
     princile = float(input("Enter the principle amount: "))
-    if principle <= 0:
-        print("Principle can't be less than or equal to zero")
+    if principle < 0:
+        print("Principle can't be less than zero")
 
-while rate <= 0:
-    rate = float(input("Enter the rate amount: "))
-    if rate <= 0:
-        print("Rate can't be less than or equal to zero")
+while rate < 0:
+    rate = float(input("Enter the interest rate: "))
+    if rate < 0:
+        print("Interest rate can't be less than zero")
 
-print(principle)
+while time < 0:
+    time = int(input("Enter the time in years: "))
+    if time < 0:
+        print("Time can't be less than zero")
+
+total = principle * pow((1 + rate / 1000), time)
+print(f"Balance after {time} year/s: ${total:.2f}")
