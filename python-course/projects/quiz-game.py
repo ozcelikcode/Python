b0bb72c21ba168237a9a9c1b3ef0f2b6dc7cdf0b
ozcelikcode <75310949+ozcelikcode@users.noreply.github.com>
2026-06-03 17:17:06 +0300

    Update quiz-game.py

--- python-course/projects/quiz-game.py
+++ python-course/projects/quiz-game.py
@@ -6,11 +6,11 @@ questions = ("How many elements are in the periodic table?: ",
              "How many bones are in the human body?: ",
              "Which planet in the solar system in the hottest?:")
 
-options = ((""),
-           (""),
-           (""),
-           (""),
-           (""))
+options = (("A. 116", "B. 117", "C. 118", "D. 119"),
+           ("A. Whale", "B. Crocodile", "C. Elephant", "D. Ostrich"),
+           ("A. Nitrogen", "B. Oxygen", "C. Carbon-Dioxide", "D. Hydrogen"),
+           ("A. 206", "B. 207", "C. 208", "D. 209"),
+           ("A. Mercury", "B. Venus", "C. Earth", "D. Mars"))
 
 answers = ("", "", "", "", "")
 guesses = []
