505e3b4f161f178b153ecc104b8cbb15301a9812
ozcelikcode <75310949+ozcelikcode@users.noreply.github.com>
2026-06-03 17:07:49 +0300

    Create quiz-game.py

---
+++ python-course/projects/quiz-game.py
@@ -0,0 +1,16 @@
+# Python Quiz Game
+
+questions = ("How many elements are in the periodic table?: ",
+             "Which animal lays the largest eggs?: ",
+             "What is the most abundant gas in Earth's atmosphere?: ",
+             "How many bones are in the human body?: ",
+             "Which planet in the solar system in the hottest?:")
+
+options = ((""),
+           (""),
+           (""),
+           (""),
+           (""))
+
+answers = ("", "", "", "", "")
+guesses = []
