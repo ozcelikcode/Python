d1ae8f12676a65d2eea2d2299a8fcc4418d31bee
ozcelikcode <75310949+ozcelikcode@users.noreply.github.com>
2026-06-03 17:52:09 +0300

    Update quiz-game.py

--- python-course/projects/quiz-game.py
+++ python-course/projects/quiz-game.py
@@ -12,5 +12,11 @@ options = (("A. 116", "B. 117", "C. 118", "D. 119"),
            ("A. 206", "B. 207", "C. 208", "D. 209"),
            ("A. Mercury", "B. Venus", "C. Earth", "D. Mars"))
 
-answers = ("", "", "", "", "")
+answers = ("C", "D", "A", "A", "B")
 guesses = []
+score = 0
+question_num = 0
+
+for question in questions:
+    print("----------")
+    print(question)
