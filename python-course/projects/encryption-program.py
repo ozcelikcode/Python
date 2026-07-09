acdfc39606b46ce89a2edc941aeb55713f00f9e4
ozcelikcode <75310949+ozcelikcode@users.noreply.github.com>
2026-07-09 12:34:17 +0300

    Update encryption-program.py

--- python-course/projects/encryption-program.py
+++ python-course/projects/encryption-program.py
@@ -7,5 +7,30 @@ key = chars.copy()
 
 random.shuffle(key)
 
-print(f"Chars: {chars}")
-print(f"Key: {key}")
+# print(f"Chars: {chars}")
+# print(f"Key: {key}")
+
+# Encrypt
+
+plain_text = input("Enter the text to be encrypted: ")
+cipher_text = ""
+
+for letter in plain_text:
+    index = chars.index(letter)
+    cipher_text += key[index]
+
+print(f"Original text: {plain_text}")
+print(f"Cipher text: {cipher_text}")
+
+
+# Decrypt
+
+cipher_text = input("Enter the text to be encrypted: ")
+plain_text = ""
+
+for letter in cipher_text:
+    index = key.index(letter)
+    plain_text += chars[index]
+
+print(f"Cipher text: {cipher_text}")
+print(f"Original text: {plain_text}")
