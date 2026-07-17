c5a843ab4bdc75a3c5d7322f02e1a700ebf6b696
ozcelikcode <75310949+ozcelikcode@users.noreply.github.com>
2026-07-17 17:50:41 +0300

    Update hangman-game.py

--- python-course/projects/hangman-game.py
+++ python-course/projects/hangman-game.py
@@ -27,5 +27,28 @@ hangman_art = {
     6: (" o ",
         "/|\\",
         "/ \\")
-
 }
+
+def display_man(wrong_guesses):
+    pass
+
+def display_hint(hint):
+    pass
+
+def display_answer(answer):
+    pass
+
+def main():
+    answer = random.choice(words)
+    hint = ["_"] * len(answer)
+    wrong_guesses = 0
+    guessed_letters = set()
+    is_running = True
+
+    while is_running:
+        display_man(wrong_guesses)
+        display_hint(hint)
+        guess = input("Enter a letter: ").lower()
+
+if __name__ == '__main__':
+    main()
