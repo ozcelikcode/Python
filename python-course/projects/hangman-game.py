ccfb3ca95036a2c004f984396450ff9bb308a8b5
ozcelikcode <75310949+ozcelikcode@users.noreply.github.com>
2026-07-17 16:52:15 +0300

    Update hangman-game.py

---
+++ python-course/projects/hangman-game.py
@@ -0,0 +1,31 @@
+# Hangman in Python
+
+import random
+
+words = ("apple", "orange", "banana", "coconut", "pineapple")
+
+# dictionary of key:()
+hangman_art = {
+    0: ("   ",
+        "   ",
+        "   "),
+    1: (" o ",
+        "   ",
+        "   "),
+    2: (" o ",
+        " | ",
+        "   "),
+    3: (" o ",
+        "/| ",
+        "  "),
+    4: (" o ",
+        "/|\\",
+        "  "),
+    5: (" o ",
+        "/|\\",
+        "/ "),
+    6: (" o ",
+        "/|\\",
+        "/ \\")
+
+}
